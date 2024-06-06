3feb2c3ee954638d11c11276e80b0490b507d0d8
oneone <you@example.com>
2024-06-06 23:48:39 +0800

    new

---
+++ chap4/4_12.py
@@ -0,0 +1,9 @@
+my_food=['pizza','falafel','carrot cake']
+print("my_food\n")
+for value in my_food:
+    print(value)
+friend_food=my_food[:]
+friend_food.append("polo pizza")
+print("friend_food \n")
+for value in friend_food:
+    print(value)
